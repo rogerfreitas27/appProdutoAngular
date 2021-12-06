46e74db59105c8801693a73ffd479a691fd1ba66
Roger Freitas <rogereduardofreitas@gmail.com>
2021-12-05 23:48:09 -0300

    upload de arquivos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormcategoriaComponent } from './categoria/formcategoria/formcategoria.component';
+import { CategoriaModule } from './categoria/categoria.module';
 
-const routes: Routes = [];
+
+
+
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full',redirectTo:'login' },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: 'categoria',     
+   loadChildren: () => import('./categoria/categoria.module').then(m => m.CategoriaModule)
+  },
+  
+  {
+    path: 'marca',
+    loadChildren: () => import('./marca/marca.module').then(m => m.MarcaModule)
+  },
+  {
+    path: 'produto',
+    loadChildren: () => import('./produto/produto.module').then(m => m.ProdutoModule)
+  },
+  
+  {
+    path: 'usuario',
+    loadChildren: () => import('./usuario/usuario.module').then(m => m.UsuarioModule)
+  }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)], 
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
+
+//import { MenuComponent } from './menu';
+//import { MarcaService } from './marca/service/marca.service';
+
+
+
+import { ComponentsModule } from './shared/components/components.module';
+import { SharedModule } from './shared/components/shared.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
+    
+      
+  ], 
   imports: [
+    AppRoutingModule,
+   // RouterModule,
     BrowserModule,
-    AppRoutingModule
+    ComponentsModule,
+    SharedModule
+    
+    
+   
+  
   ],
-  providers: [],
+  providers: [
+    //CategoriaService,MarcaService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
